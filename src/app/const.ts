65bed3031526028cc0ed42e1a81c6e3b76031671
RedTedRedemption <teddy.herzfeld@icloud.com>
2018-07-15 16:09:46 -0400

    person object
    +person object
    +changed tab1 to use person object
    +added test person objects to const.ts

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,12 +1,20 @@
-import {anon} from "./anon";
+import { anon } from "./anon";
 import { Tab } from "./tab";
+import { Person } from "./person";
 
 export const TESTOBJS: anon[] = [
-{name: "john", subinfo: "test1"},
-{name: "jane", subinfo: "test2"}
+  { name: "john", subinfo: "test1" },
+  { name: "jane", subinfo: "test2" }
 ];
 
 export const TABS: Tab[] = [
-{name: "tab1" path: ""},
-{name: "tab2" path: "/tab2"}
+  { name: "tab1", path: "" },
+  { name: "tab2", path: "/tab2" }
+];
+
+export const PEOPLE: Person[] = [
+  { name: "john doe", detail: "a human...more or less" },
+  { name: "jane doe", detail: "a human...less or more" },
+  { name: "silus", detail: "nephew" },
+  { name: "julian", detail: "father" }
 ];
--- src/app/tab-content/tab-content.component.ts
+++ src/app/tab-content/tab-content.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {TESTOBJS} from '../const';
-import {object} from '../testobj';
+import { Component, OnInit } from "@angular/core";
+import { TESTOBJS } from "../const";
+import { object } from "../testobj";
+import { PEOPLE } from "../const";
+import { Person } from "../person";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-tab-content',
-  templateUrl: './tab-content.component.html',
-  styleUrls: ['./tab-content.component.css']
+  selector: "app-tab-content",
+  templateUrl: "./tab-content.component.html",
+  styleUrls: ["./tab-content.component.css"]
 })
 export class TabContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  router = Router;
 
   objects = TESTOBJS;
-  cont = 'tab1';
+  cont = "tab1";
 
+  people = PEOPLE;
 
+  selectedPerson: Person;
 
-
-  ngOnInit() {
+  selectPerson(selPerson: Person) {
+    this.selectedPerson = selPerson;
   }
 
+  ngOnInit() {}
 }
